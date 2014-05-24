12e89f67d0cf291e0e43a64ed54a0d0fcd65b0dd
Torgeir <torgeir@helgevold-consulting.com>
2014-05-24 18:48:59 -0400

    easier bootstrapping

---
+++ ExpressUnitJS/Framework/initialize.js
@@ -0,0 +1,15 @@
+﻿
+function RunAllTests(){
+    require(['Framework/testRunner', 'Framework/testContext'], function (runner, testContext) {
+
+        $.ajaxSetup({ cache: false });
+
+        require.config({
+            urlArgs: "bust=" + (new Date()).getTime()
+        });
+
+        runner.runTests();
+    });
+}
+   
+RunAllTests();

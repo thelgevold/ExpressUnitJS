13f8c0386addf339d334e5af329fb1fc80d2b1f5
Torgeir <torgeir@helgevold-consulting.com>
2014-05-24 19:36:59 -0400

    added tests

--- ExpressUnitJS/Tests/TestFixtureTreeNodeTests.js
+++ ExpressUnitJS/Tests/TestFixtureTreeNodeTests.js
@@ -1,11 +1,29 @@
 ﻿define(['Framework/assert', 'Framework/test'], function (assert, test) {
 
-    window.testContext.fixtures.TestFixtureTreeNodeTests.tests.push(new test("Test1a", function () {
-        assert.areEqual(1, 1);
+    window.testContext.fixtures.TestFixtureTreeNodeTests.tests.push(new test("WillGetCorrectPassedCount", function () {
+        require(["Framework/testFixtureTreeNode"], function (testFixtureTreeNode) {
+            var node = testFixtureTreeNode();
+            var items = [];
+            items.push({ passed: true });
+            items.push({ passed: false });
+            items.push({ passed: true });
+
+            var count = node.getCount(true, items);
+            assert.areEqual(2, count);
+        });
     }));
 
-    window.testContext.fixtures.TestFixtureTreeNodeTests.tests.push(new test("Test2b", function () {
-        assert.areEqual(1, 41);
+    window.testContext.fixtures.TestFixtureTreeNodeTests.tests.push(new test("WillGetCorrectFailedCount", function () {
+        require(["Framework/testFixtureTreeNode"], function (testFixtureTreeNode) {
+            var node = testFixtureTreeNode();
+            var items = [];
+            items.push({ passed: true });
+            items.push({ passed: false });
+            items.push({ passed: true });
+
+            var count = node.getCount(false, items);
+            assert.areEqual(1, count);
+        });
     }));
 
     return window.testContext.fixtures.TestFixtureTreeNodeTests;

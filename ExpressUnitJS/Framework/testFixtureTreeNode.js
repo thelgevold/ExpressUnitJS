3fee9db3302ec5f34771cae20fca54f46faa6998
Torgeir <torgeir@helgevold-consulting.com>
2014-05-24 17:24:07 -0400

    added badges

--- ExpressUnitJS/Framework/testFixtureTreeNode.js
+++ ExpressUnitJS/Framework/testFixtureTreeNode.js
@@ -4,10 +4,41 @@
         this.testFixtureName = testFixtureName;
         this.tests = ko.observableArray([]);
 
-        this.passedCount = 0;
-        this.failedCount = 0;
+        var self = this;
 
+        this.passedCount = ko.computed(function () {
+            return self.getCount(true,this.tests());
+        }, self);
 
+        this.failedCount = ko.computed(function () {
+            return self.getCount(false,this.tests());
+        }, self);
+
+        this.icon = ko.observable("treeNodeIcon glyphicon glyphicon-minus");
+
+        this.showTests = ko.observable(true);
+    };
+
+    testFixtureTreeNode.prototype.expandColapse = function () {
+        this.showTests(!this.showTests());
+
+        if (this.showTests() === false) {
+            this.icon("treeNodeIcon glyphicon glyphicon-plus");
+        }
+        else {
+            this.icon("treeNodeIcon glyphicon glyphicon-minus");
+        }
+    };
+
+    testFixtureTreeNode.prototype.getCount = function(value,items){
+        var count = 0;
+        for(var i = 0; i < items.length; i++){
+            if(items[i].passed === value){
+                count++;
+            }
+        }
+
+        return count;
     };
 
     return testFixtureTreeNode;

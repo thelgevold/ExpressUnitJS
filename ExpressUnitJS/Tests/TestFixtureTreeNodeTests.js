d1fb805682ddd9f43b6f9f0badf404d3a4898473
Torgeir <torgeir@helgevold-consulting.com>
2014-05-24 21:07:06 -0400

    fixed tests

--- ExpressUnitJS/Tests/TestFixtureTreeNodeTests.js
+++ ExpressUnitJS/Tests/TestFixtureTreeNodeTests.js
@@ -1,29 +1,25 @@
-﻿define(['Framework/assert', 'Framework/test'], function (assert, test) {
+﻿define(['Framework/assert', 'Framework/test', 'Framework/testFixtureTreeNode'], function (assert, test, testFixtureTreeNode) {
 
     window.testContext.fixtures.TestFixtureTreeNodeTests.tests.push(new test("WillGetCorrectPassedCount", function () {
-        require(["Framework/testFixtureTreeNode"], function (testFixtureTreeNode) {
-            var node = testFixtureTreeNode();
-            var items = [];
-            items.push({ passed: true });
-            items.push({ passed: false });
-            items.push({ passed: true });
+        var node = new testFixtureTreeNode();
+        var items = [];
+        items.push({ passed: true });
+        items.push({ passed: false });
+        items.push({ passed: true });
 
-            var count = node.getCount(true, items);
-            assert.areEqual(2, count);
-        });
+        var count = node.getCount(true, items);
+        assert.areEqual(2, count);
     }));
 
     window.testContext.fixtures.TestFixtureTreeNodeTests.tests.push(new test("WillGetCorrectFailedCount", function () {
-        require(["Framework/testFixtureTreeNode"], function (testFixtureTreeNode) {
-            var node = testFixtureTreeNode();
-            var items = [];
-            items.push({ passed: true });
-            items.push({ passed: false });
-            items.push({ passed: true });
+        var node = new testFixtureTreeNode();
+        var items = [];
+        items.push({ passed: true });
+        items.push({ passed: false });
+        items.push({ passed: true });
 
-            var count = node.getCount(false, items);
-            assert.areEqual(1, count);
-        });
+        var count = node.getCount(false, items);
+        assert.areEqual(1, count);
     }));
 
     return window.testContext.fixtures.TestFixtureTreeNodeTests;
